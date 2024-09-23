2ee69ca7b1e62958c7676cba084c03a8a891c5a0
cuong_nl <luongcuong244@gmail.com>
2024-09-23 22:04:25 +0700

    e

---
+++ app/src/main/java/com/kma/musicplayerv2/network/retrofit/model/ExploreResponse.kt
@@ -0,0 +1,3 @@
+package com.kma.musicplayerv2.network.retrofit.model
+
+data class ExploreResponse()
---
+++ app/src/main/java/com/kma/musicplayerv2/network/retrofit/repository/ExploreRepository.kt
@@ -0,0 +1,21 @@
+package com.kma.musicplayerv2.network.retrofit.repository
+
+import com.kma.musicplayerv2.network.retrofit.model.LoginRequest
+import com.kma.musicplayerv2.network.retrofit.model.LoginResponse
+import com.kma.musicplayerv2.network.retrofit.model.RegisterRequest
+import com.kma.musicplayerv2.network.retrofit.RetrofitClient
+import com.kma.musicplayerv2.network.retrofit.api.AuthApi
+import retrofit2.Callback
+
+object AuthRepository {
+
+    private val authApi: AuthApi = RetrofitClient.getClient().create(AuthApi::class.java)
+
+    fun login(loginRequest: LoginRequest, callback: Callback<LoginResponse>) {
+        authApi.login(loginRequest).enqueue(callback)
+    }
+
+    fun register(registerRequest: RegisterRequest, callback: Callback<Boolean>) {
+        authApi.register(registerRequest).enqueue(callback)
+    }
+}
